57e44dc2620000d9cca7f3040c9385b4e765f9ae
piyush-sh19 <piyush.sh19@gmail.com>
2020-08-16 23:41:12 +0530

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,20 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="app">
+        <div className="app__top">
+          <img
+            className="app__logo"
+            src="https://instagram-brand.com/wp-content/themes/ig-branding/assets/images/ig-logo-email.png"
+            alt="Insta logos"
+          />
+          <h1>Reels</h1>
+        </div>
+        <div className="app__videos">
+          
+        </div>
     </div>
   );
 }
